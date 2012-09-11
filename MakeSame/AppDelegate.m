894665a697b8fb42bc8d6eaba22f7d8b10bf307d
Onur Avun <onuravun@yandex.com>
2012-09-11 16:28:14 +0300

    Licensing

--- MakeSame/AppDelegate.m
+++ MakeSame/AppDelegate.m
@@ -1,10 +1,25 @@
-//
-//  AppDelegate.m
-//  MakeSame
-//
-//  Created by Onur Avun on 11.09.2012.
-//  Copyright (c) 2012 Cellenity. All rights reserved.
-//
+/*
+ * AppDelegate.m
+ * MakeSame
+ *
+ * Created by Onur Avun.
+ * Copyright 2012, Onur Avun.
+ *
+ *
+ * MakeSame is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU  General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * MakeSame is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU  General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #import "AppDelegate.h"
 
